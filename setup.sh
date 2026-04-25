c5826a5f198e4ef1c56d2efc3379daa69df77f7c
Darren Chang <darrenchang951@gmail.com>
2026-04-24 18:57:50 -0700

    Install openclaw

--- setup.sh
+++ setup.sh
@@ -1,24 +1,34 @@
-#!/bin/zsh
+#!/bin/bash
 
 set -e
 
+apt update
+apt -y install curl tmux
+
 # Install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash && \
-source ~/.zshrc && \
+source ~/.bashrc && \
 nvm install 24 && \
 nvm use 24;
 
 # Install ollama
-sudo apt install -y zstd pciutils && \
+apt install -y zstd pciutils && \
 if which ollama >/dev/null 2>&1; then
   echo "ollama is installed";
 else
   echo "ollama not found" && \
   curl -fsSL https://ollama.com/install.sh | sh;
+  source ~/.bashrc;
+  ollama serve 2>&1 > /dev/null &
+  ollama pull gemma4
+  ollama pull gemma4:26b
 fi
-ollama pull gemma4:26b
-ollama pull gemma4:31b
-ollama pull qwen3:235b
+#ollama pull gemma4:31b
+#ollama pull qwen3:235b
+
+# Install homebrew
+apt install -y git
+curl -fsSL "https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh" | bash
 
 # Install openclaw
 if which openclaw >/dev/null 2>&1; then
@@ -26,4 +36,10 @@ if which openclaw >/dev/null 2>&1; then
 else
   echo "openclaw not found" && \
   npm install -g openclaw@latest
-
+  npm install -g @larksuiteoapi/node-sdk
+  npm install -g nostr-tools
+  npm install -g @slack/web-api
+  npm install -g jimp
+  npm install -g @whiskeysockets/baileys
+  source ~/.bashrc;
+fi
